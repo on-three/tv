24920e73e6077957e2fae36abab34d449ab674a7
on-three <on.three.email@gmail.com>
2020-01-05 09:41:45 -0800

    Correct recording path

--- record.sunday.baroque.sh
+++ record.sunday.baroque.sh
@@ -7,8 +7,6 @@ URL="https://14073.live.streamtheworld.com/KCNVFM_SC"
 # 5AM-12PM--> 7 hours in seconds
 LENGTH_SECONDS=25200
 
-OUTFILE=${HOME}/sata/incoming/${SHOW_NAME}.${DATE}.mp3
+OUTFILE=${HOME}/Videos/${SHOW_NAME}.${DATE}.mp3
 
 curl --output ${OUTFILE} --max-time ${LENGTH_SECONDS} --insecure  ${URL}
-
-
